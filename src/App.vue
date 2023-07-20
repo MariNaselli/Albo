35114d579c49feef990ceb68790433ebfc212d3f
Mariana Naselli <nasellimariana@gmail.com>
2023-07-20 05:00:59 +0200

    Traslado todo a la main

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <AlboView />
+  </div>
+</template>
+
+<script>
+import AlboView from './components/AlboView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AlboView
+  }
+}
+</script>
+
+<style>
+
+</style>
